82b1570095f7a60ae4ff5343ab3cf58a6cd6b481
Gabe Anzelini <me@gabeanzelini.com>
2010-12-28 16:09:26 -0800

    added teardown to IntegrationClass to reset Capybara sessions

--- test/support/integration_case.rb
+++ test/support/integration_case.rb
@@ -2,4 +2,8 @@
 class ActiveSupport::IntegrationCase < ActiveSupport::TestCase
   include Capybara
   include Rails.application.routes.url_helpers
+  
+  def teardown
+    Capybara.reset_sessions!
+  end
 end
