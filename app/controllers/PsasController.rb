26394e31f313ffa55366a9d1457c75829d8bbb16
Gabe Anzelini <me@gabeanzelini.com>
2010-12-29 21:32:50 -0800

    testing contoller issues

--- app/controllers/PsasController.rb
+++ app/controllers/PsasController.rb
@@ -1,6 +1,6 @@
-class PsasController < InheritedResources::Base
-  include InheritedResources::DSL
-  create! do |success,failure|
-    success.html{ redirect_to psas_path }
-  end
+class PsasController < ApplicationContoller #InheritedResources::Base
+  #include InheritedResources::DSL
+  #create! do |success,failure|
+  #  success.html{ redirect_to psas_path }
+  #end
 end
